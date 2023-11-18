53a359412b693db563810ce3d1c54b2ed57dfe49
Haris <118927126+harisA1@users.noreply.github.com>
2023-11-18 16:53:57 +0200

    Update project_config.py

--- configs/project_config.py
+++ configs/project_config.py
@@ -6,9 +6,9 @@ from pathlib import Path
 class ProjectConfig:
     project_root = Path(__file__).absolute().parent.parent
     single_gpu = 1  # TODO: which gpu to run
-    # device = torch.device(f"cuda:{single_gpu}")
-    device = torch.device(f"cpu")
-    # torch.cuda.set_device(device)
+    device = torch.device(f"cuda:{single_gpu}")
+    # device = torch.device(f"cpu")
+    torch.cuda.set_device(device)
     num_workers = 0  # Number of threads for loading data
     run_root = Path("./runs")  # Save each run
     final_data_root = Path("./final_data")  # Cache preprocessed data
